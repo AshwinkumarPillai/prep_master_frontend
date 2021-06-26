d8f6cb0f02445ff352ad6dd54536fb5482e9b952
AshwinkumarPillai <ashwinfury1@gmail.com>
2021-06-26 18:50:18 +0530

    multiple choice indication added

--- src/pages/question.js
+++ src/pages/question.js
@@ -14,9 +14,9 @@ const question = (props) => {
 
   return (
     <React.Fragment>
-      <div className="card test_question_card orange lighten-3" style={{ padding: "20px" }}>
+      <div className="card test_question_card indigo lighten-5" style={{ padding: "20px", backgroundColor: "white" }}>
         <div className="row">
-          <div className="col s12">
+          <div className="col s12 ">
             <h6>
               <span className="new badge black left" data-badge-caption="." style={{ marginRight: "8px" }}>
                 {props.index}
@@ -25,53 +25,59 @@ const question = (props) => {
             </h6>
           </div>
         </div>
+
         <hr style={{ border: ".5px solid black" }} />
-        {props.question.multiCorrect ? (
-          <React.Fragment>
-            {props.question.options.map((option, index2) => (
-              <React.Fragment key={index2}>
-                <p className="ptest">
-                  <label>
-                    <input
-                      id={option.value + index2}
-                      type="checkbox"
-                      className="filled-in test_option_checkbox"
-                      checked={props.selectedOptions !== undefined && props.selectedOptions.has(option._id)}
-                      onChange={() => selectOption(option._id)}
-                    />
-                    <span></span>
-                  </label>
-                  <label htmlFor={option.value + index2} className="customLabel">
-                    {option.value}
-                  </label>
-                </p>
-              </React.Fragment>
-            ))}
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            {props.question.options.map((option, index2) => (
-              <React.Fragment key={index2}>
-                <p className="ptest">
-                  <label>
-                    <input
-                      type="radio"
-                      id={props.question.title + index2}
-                      name={props.question.title}
-                      className="with-gap test_option_radio"
-                      checked={props.selectedOptions !== undefined && props.selectedOptions.has(option._id)}
-                      onChange={() => selectOption(option._id)}
-                    />
-                    <span></span>
-                  </label>
-                  <label htmlFor={props.question.title + index2} className="customLabel">
-                    {option.value}
-                  </label>
-                </p>
-              </React.Fragment>
-            ))}
-          </React.Fragment>
-        )}
+        <div>
+          {props.question.multiCorrect ? (
+            <React.Fragment>
+              <p className="multiplAnsDesc" style={{ color: "red", fontWeight: "bold" }}>
+                * Multiple answers possible
+              </p>
+              {props.question.options.map((option, index2) => (
+                <React.Fragment key={index2}>
+                  <p className="ptest">
+                    <label>
+                      <input
+                        id={option.value + index2}
+                        type="checkbox"
+                        className="filled-in test_option_checkbox"
+                        checked={props.selectedOptions !== undefined && props.selectedOptions.has(option._id)}
+                        onChange={() => selectOption(option._id)}
+                      />
+                      <span></span>
+                    </label>
+                    <label htmlFor={option.value + index2} className="customLabel">
+                      {option.value}
+                    </label>
+                  </p>
+                </React.Fragment>
+              ))}
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {props.question.options.map((option, index2) => (
+                <React.Fragment key={index2}>
+                  <p className="ptest">
+                    <label>
+                      <input
+                        type="radio"
+                        id={props.question.title + index2}
+                        name={props.question.title}
+                        className="with-gap test_option_radio"
+                        checked={props.selectedOptions !== undefined && props.selectedOptions.has(option._id)}
+                        onChange={() => selectOption(option._id)}
+                      />
+                      <span></span>
+                    </label>
+                    <label htmlFor={props.question.title + index2} className="customLabel">
+                      {option.value}
+                    </label>
+                  </p>
+                </React.Fragment>
+              ))}
+            </React.Fragment>
+          )}
+        </div>
 
         <br />
       </div>
