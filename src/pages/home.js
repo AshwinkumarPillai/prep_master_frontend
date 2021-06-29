1c007b72562123e1dd040888ee5cf427f35dadb4
AshwinkumarPillai <ashwinfury1@gmail.com>
2021-06-29 14:11:51 +0530

    Test fetching and loading states updated properly

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ class Home extends Component {
   constructor(props) {
     super(props);
     let loggedIn = localStorage.getItem("user") ? true : false;
-    this.state = { tests: [], loggedIn };
+    this.state = { testsLoaded: false, tests: [], loggedIn };
   }
 
   componentDidMount() {
@@ -15,8 +15,13 @@ class Home extends Component {
   }
 
   async fetchTests() {
-    let resp = await fetchAllTests();
-    if (resp.data.status === 200) await this.setState({ tests: resp.data.tests });
+    try {
+      let resp = await fetchAllTests();
+      if (resp.data.status === 200) await this.setState({ tests: resp.data.tests });
+      this.setState({ testsLoaded: true });
+    } catch (error) {
+      this.setState({ testsLoaded: true });
+    }
   }
 
   selectTest = (testId) => {
@@ -32,38 +37,42 @@ class Home extends Component {
       <React.Fragment>
         <div className="container" style={{ marginTop: "10vh" }}>
           <div className="row">
-            {this.state.tests.length > 0 ? (
-              this.state.tests.map((test, key) => {
-                return (
-                  <div
-                    className="col s12 m4 z-depth-3 testCard"
-                    key={key}
-                    onClick={() => this.selectTest(test._id)}
-                    style={{ cursor: "pointer" }}
-                  >
-                    <div className="card indigo darken-1">
-                      <div className="card-content white-text">
-                        <span className="card-title" style={{ color: "#ffea00", textTransform: "capitalize" }}>
-                          {test.name}
-                        </span>
-                        <p>
-                          <span style={{ color: "#f48fb1" }}>Number of questions :</span> {test.questions.length}
-                        </p>
-                        <p>
-                          <span style={{ color: "#f48fb1" }}>Time Limit: </span>
-                          {test.time_limit === -1 ? "No limit" : `${test.time_limit} minutes`}
-                        </p>
-                        {!this.state.loggedIn ? (
-                          <span>
-                            <i className="material-icons right">lock</i>
-                            <br />
-                          </span>
-                        ) : null}
-                      </div>
-                    </div>
-                  </div>
-                );
-              })
+            {this.state.testsLoaded ? (
+              <React.Fragment>
+                {this.state.tests.length
+                  ? this.state.tests.map((test, key) => {
+                      return (
+                        <div
+                          className="col s12 m4 z-depth-3 testCard"
+                          key={key}
+                          onClick={() => this.selectTest(test._id)}
+                          style={{ cursor: "pointer" }}
+                        >
+                          <div className="card indigo darken-1">
+                            <div className="card-content white-text">
+                              <span className="card-title" style={{ color: "#ffea00", textTransform: "capitalize" }}>
+                                {test.name}
+                              </span>
+                              <p>
+                                <span style={{ color: "#f48fb1" }}>Number of questions :</span> {test.questions.length}
+                              </p>
+                              <p>
+                                <span style={{ color: "#f48fb1" }}>Time Limit: </span>
+                                {test.time_limit === -1 ? "No limit" : `${test.time_limit} minutes`}
+                              </p>
+                              {!this.state.loggedIn ? (
+                                <span>
+                                  <i className="material-icons right">lock</i>
+                                  <br />
+                                </span>
+                              ) : null}
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })
+                  : "No tests available at this moment"}
+              </React.Fragment>
             ) : (
               <div className="center"> Loading...</div>
             )}
