a226653a568420bae00bf57db7b653003c3a4228
AshwinkumarPillai <ashwinfury1@gmail.com>
2021-06-29 14:27:22 +0530

    new api routes added

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,18 +1,23 @@
 import httpClient from "./httpClient";
 
 // User and Admin
-export const adminLogin = (payload) => httpClient.post("admin/login", payload);
 export const login = (payload) => httpClient.post("user/login", payload);
 export const register = (payload) => httpClient.post("user/register", payload);
+export const adminLogin = (payload) => httpClient.post("admin/login", payload);
 export const authAdmin = (payload) => httpClient.post("admin/auth", payload);
 
 // Test
 export const fetchAllTests = () => httpClient.get("/test/fetchAllTests", {});
 export const fetchTestDetails = (payload) => httpClient.get(`/test/fetchTest?q=${payload}`);
+export const fetchAllAdminTests = () => httpClient.get("/test/fetchAllAdminTests", {});
 export const fetchFullTestDetails = (payload) => httpClient.get(`/test/fetchFullTest?q=${payload}`);
 export const createTest = (payload) => httpClient.post("/test/add", payload);
 export const updateTest = (payload) => httpClient.post("/test/update", payload);
-export const deleteTest = (payload) => httpClient.post("/test/delete", payload);
+export const deleteTest = (payload) => httpClient.post("/test/delete", payload); //archives the test doesn't delete it completely
+export const restoreTest = (payload) => httpClient.post("/test/restore", payload);
+export const publishTest = (payload) => httpClient.post("/test/publish", payload);
+export const fetchArchivedTests = () => httpClient.get("/test/getArchivedTests");
+export const fetchArchivedTestDetails = () => httpClient.get("/test/fetchArchivedTest");
 
 // Question
 export const addQuestion = (payload) => httpClient.post("/question/add", payload, { headers: { "Content-Type": "multipart/form-data" } });
