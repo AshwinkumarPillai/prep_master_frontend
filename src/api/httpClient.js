ec478bf07b972bb31edf9d512471707655b77aec
Ashwinkumar Pillai <ashwin@Ashwinkumars-MacBook-Pro.local>
2022-12-18 00:40:10 -0500

    Added Backend URL

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_URL || "http://localhost:3200/";
+// const baseURL = process.env.REACT_APP_URL || "http://localhost:3200/";
+const baseURL = "https://prep-master-backend.onrender.com/";
 
 const httpClient = axios.create({
   baseURL,
